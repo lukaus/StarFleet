7894d656f966ea86b6db42be4544d6781ab77889
Samuel West <swest@MacBook-Pro-6.local>
2018-01-27 16:17:34 -0600

    Alpha 1 of Projectiles

--- Projectile.cpp
+++ Projectile.cpp
@@ -1,3 +1,12 @@
 #include "Projectile.h"
 
+Projectile::Projectile()
+{
+    x_pos = 0;
+    y_pos = 0;
+}
+
+Projectile::Projectile(int roll, int x_pos, int y_pos, Orientation orientation)
+{
+}
 // mutators and accessors
