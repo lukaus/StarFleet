4344894384874e3a982c986707f8ea2b4fd18b0b
Jordan Flowers <jordanflowers00@gmail.com>
2018-01-27 16:34:46 -0500

    The good server

--- server.cpp
+++ server.cpp
@@ -28,7 +28,6 @@ int main(int argc , char *argv[])
     int client_socket[30];
     int max_clients = 30;
     int activity;
-    int i;
     int valread;
     int sd;
     int numConnected;
@@ -46,7 +45,7 @@ int main(int argc , char *argv[])
     char *message = "";  
     
     //initialise all client_socket[] to 0 so not checked 
-    for (i = 0; i < max_clients; i++)  
+    for (int i = 0; i < max_clients; i++)  
     {  
         client_socket[i] = 0;  
     }  
@@ -79,7 +78,7 @@ int main(int argc , char *argv[])
         exit(EXIT_FAILURE);  
     }  
     printf("Listener on port %d \n", PORT);  
-        
+    
     //try to specify maximum of 3 pending connections for the master socket 
     if (listen(master_socket, 3) < 0)  
     {  
@@ -93,7 +92,6 @@ int main(int argc , char *argv[])
     numConnected = 0;
     while(TRUE)  
     {  
-        cerr << "error: " << errno << endl;
         //clear the socket set 
         FD_ZERO(&readfds);  
     
@@ -102,7 +100,7 @@ int main(int argc , char *argv[])
         max_sd = master_socket;  
             
         //add child sockets to set 
-        for ( i = 0 ; i < max_clients ; i++)  
+        for (int i = 0 ; i < max_clients ; i++)  
         {  
             //socket descriptor 
             sd = client_socket[i];  
@@ -119,7 +117,7 @@ int main(int argc , char *argv[])
         //wait for an activity on one of the sockets , timeout is NULL , 
         //so wait indefinitely 
         activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);  
-        numConnected++;
+        
         if ((activity < 0) && (errno!=EINTR))  
         {  
             printf("select error");  
@@ -129,12 +127,13 @@ int main(int argc , char *argv[])
         //then its an incoming connection 
         if (FD_ISSET(master_socket, &readfds))  
         {  
+
             if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)  
             {  
                 perror("accept");  
                 exit(EXIT_FAILURE);  
             }  
-            
+            numConnected++;
             //inform user of socket number - used in send and receive commands 
             printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));  
             
@@ -143,10 +142,10 @@ int main(int argc , char *argv[])
             {  
                 perror("send");  
             }  
-                
+            
             puts("Welcome message sent successfully");  
             //add new socket to array of sockets 
-            for (i = 0; i < max_clients; i++)  
+            for (int i = 0; i < max_clients; i++)  
             {  
                 //if position is empty 
                 if( client_socket[i] == 0 )  
@@ -157,32 +156,34 @@ int main(int argc , char *argv[])
                     break;  
                 }  
             }  
+
         }  
             
         //else its some IO operation on some other socket
-        for (i = 0; i < max_clients; i++)  
+        for (int i = 0; i < max_clients; i++)  
         {  
             sd = client_socket[i];  
             if (FD_ISSET( sd , &readfds))  
             {  
+
                 //Check if it was for closing , and also read the 
                 //incoming message 
-                if ((valread = read( sd , buffer, 1024)) == 0)  
+                if ((valread = read( sd , buffer, 99999)) == 0)  
                 {  
                     //Somebody disconnected , get his details and print 
-                    getpeername(sd , (struct sockaddr*)&address , \
-                        (socklen_t*)&addrlen);  
+                    getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);  
                     printf("Host disconnected , ip %s , port %d \n" , 
                           inet_ntoa(address.sin_addr) , ntohs(address.sin_port));  
-                        
+                    
                     //Close the socket and mark as 0 in list for reuse 
                     close( sd );  
                     client_socket[i] = 0;
                 }  
                     
-                //Echo back the message that came in 
+                //Echo back the message that came in to all clients
                 else
                 {  
+                    
                     //set the string terminating NULL byte on the end 
                     //of the data read 
                     //valread is  the number of values read
@@ -192,9 +193,11 @@ int main(int argc , char *argv[])
                     }
                     buffer[0] = to_string(sd)[0];
                     buffer[valread+1] = '\0';
-                    for (int i = 0; i < numConnected; i++)
+                    for (int i = 0; i < max_clients; i++)
                     {
-                        send(client_socket[i] , buffer, strlen(buffer) , 0 );
+                        if (client_socket[i] != 0)
+                            send(client_socket[i] , buffer, strlen(buffer) , 0);
+                        cerr << "Error: " << errno << endl;
                     }
                     memset(buffer, 0, sizeof(buffer));
                 }  
@@ -202,7 +205,7 @@ int main(int argc , char *argv[])
         }  
     }  
     close(master_socket);
-    for(i = 0; i < 30; i++)
+    for(int i = 0; i < 30; i++)
         close(client_socket[i]);
         
     return 0;  
